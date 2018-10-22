0d3ec09a4e84569a9ec0679579f6eeec3e7dde5f
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-22 16:05:11 -0500

    Adding component UI with JSX
    In JSX, Use 'className' instead of 'class' as class is a reserved keyword in JS
    If you want to return an element on its own line, you have to use a pair of parenthesis.
    You should return only one parent element, we can't return two sibling elements without wrapping them in a parent element.
    Capital letters is the naming convention for React components

---
+++ src/components/common/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Capital letters is the naming convention for React components
+const Header = () => {
+    return <div>Header</div>;
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/common/Header';
 
 // ES5 JS Function - So, this component is called Functional Component
+// In JSX, Use 'className' instead of 'class' as class is a reserved keyword in JS
+// If you want to return an element on its own line, you have to use a pair of parenthesis. 
+// You should return only one parent element, we can't return two sibling elements without wrapping them in a parent element.
 const App = () => {
-    return <h1>React Coin</h1>;
+    return (
+        <div>
+            <Header/>
+            <h1>React Coin</h1>
+            <p>Up to date crypo currencies financial data</p>
+        </div>
+    );
 }
 
 ReactDOM.render(
