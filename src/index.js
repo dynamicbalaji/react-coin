af4c256e4ca5d32d109dd76f188eb90b84be7435
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-22 15:42:43 -0500

    Adding first react component
    ES5 JS function is used so this component is called Functional component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert('Om Muruga')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// ES5 JS Function - So, this component is called Functional Component
+const App = () => {
+    return <h1>React Coin</h1>;
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
