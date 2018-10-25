6cb0418b3a7627fb99bbf76a36d875610860f858
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-25 10:07:38 -0500

    Adding image to a component
    import logo from './logo.png';
    <img src={logo} alt="logo" className="Header-logo"/>

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Header.css';
+import logo from './logo.png';
 
 // Capital letters is the naming convention for React components
 const Header = () => {
-    return <div className="Header">Header</div>;
+    return (<div className="Header">
+        <img src={logo} alt="logo" className="Header-logo"/>
+    </div>
+    );
 }
 
 export default Header;
