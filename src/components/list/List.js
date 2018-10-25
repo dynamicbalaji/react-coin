c4ed896534f98fe35a5cfce6962ba7866f316c10
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-25 10:44:19 -0500

    Creating class constructor, initializing state & adding conditional loading text

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 class List extends React.Component {
+    // Everytime we create a class constructor, we need to call super if they are sub classes
+    // All of our component classes are sub classes of React.Component
+    constructor(){
+        super();
+
+        this.state = {
+            loading: true,
+            currencies: [],
+            error: null
+        };
+    }
+
     render() {
+        if(this.state.loading){
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>text</div>
         );
