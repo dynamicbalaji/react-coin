c38caa3abf8b82e0a7eeb09af09afa2c0679305b
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-25 13:10:34 -0500

    Rendering few columns of received currency list in a table

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -42,11 +42,34 @@ class List extends React.Component {
         }
 
         return (
-            <div>
-                {this.state.currencies.map((currency) => (
-                    <div key={currency.id}>{currency.id}</div>
-                ))}
-            </div>
+            <div className="Table-container"> 
+            <table className="Table">
+              <thead className="Table-head">
+                <tr>
+                  <th>Cryptocurrency</th>
+                  <th>Price</th>
+                  <th>Market Cap</th>
+                  <th>24H Change</th>  
+                </tr>    
+              </thead>
+              <tbody className="Table-body">
+              {this.state.currencies.map((currency) => (
+                <tr key={currency.id}>
+                  <td>
+                    <span className="Table-rank">{currency.rank}</span>
+                    {currency.name}
+                  </td>
+                  <td>
+                    <span className="Table-dollar">$ {currency.price}</span>
+                  </td>
+                  <td>
+                    <span className="Table-dollar">$ {currency.marketCap}</span>
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
+          </div>
         );
     }
 }
