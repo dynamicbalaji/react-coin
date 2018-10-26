44527f1007ba8f61ebaceac7f0ead902cafbfa33
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-26 16:19:27 -0500

    Conditional form using state & onchange event

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -5,21 +5,37 @@ class Search extends React.Component{
     constructor(){
         super();
 
+        this.state = {
+            searchQuery: '',
+            firstName: ''
+        };
+
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleSubmit(event){
         event.preventDefault();
 
-        console.log(this.searchQuery.value);
-        console.log(this.firstName.value);
+        console.log(this.state);
+    }
+
+    handleChange(event){
+        const inputName = event.target.name;
+        const inputValue = event.target.value;
+
+        if(inputName === 'searchQuery') {
+            this.setState({searchQuery: inputValue});
+        } else if (inputName === 'firstName') {
+            this.setState({firstName: inputValue});
+        }
     }
 
     render(){
         return (
             <form onSubmit={this.handleSubmit}>
-                <input ref={(input) => this.searchQuery = input}/>
-                <input ref={(input) => this.firstName = input}/>
+                <input name="searchQuery" onChange={this.handleChange}/>
+                <input name="firstName" onChange={this.handleChange}/>
                 <button>Submit</button>
             </form>
         );
