189d7620b09d5a29b168c32d9790f592f8fec800
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-25 23:10:48 -0500

    Creating a new component for handling 404

---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className="NotFound">
+            <h1 className="NotFound-title">Oops! Page not found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
